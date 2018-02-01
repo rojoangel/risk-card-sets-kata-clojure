e49a7c894f062f25b20da428429a7544b11b1b4d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-01 22:07:19 +0100

    rewrite some collection tests in the 'should' form

--- test/risk_cards/collection_test.clj
+++ test/risk_cards/collection_test.clj
@@ -3,15 +3,18 @@
             [risk-cards.collection :as collection]))
 
 (deftest collection-of-cards-contains-a-valid-set
-  (testing "A collection of 0 cards does not contain a valid set"
-    (is (= false (collection/contains-valid-set? []))))
-  (testing "A collection of 1 cards does not contain a valid set"
-    (is (= false (collection/contains-valid-set? [\H])))
-    (is (= false (collection/contains-valid-set? [\S])))
-    (is (= false (collection/contains-valid-set? [\C])))
-    (is (= false (collection/contains-valid-set? [\J]))))
-  (testing "A collection of 2 cards does not contain a valid set"
-    (is (= false (collection/contains-valid-set? [\H \S]))))
+  (testing "A collection of 0 cards should"
+    (testing "contain a valid set"
+      (is (= false (collection/contains-valid-set? [])))))
+  (testing "A collection of 1 cards should"
+    (testing "not contain a valid set"
+      (is (= false (collection/contains-valid-set? [\H])))
+      (is (= false (collection/contains-valid-set? [\S])))
+      (is (= false (collection/contains-valid-set? [\C])))
+      (is (= false (collection/contains-valid-set? [\J])))))
+  (testing "A collection of 2 cards"
+    (testing "should not contain a valid set"
+      (is (= false (collection/contains-valid-set? [\H \S])))))
   (testing "A collection of 3 cards should"
     (testing "contain a valid set if all cards are equal"
       (is (= true (collection/contains-valid-set? [\H \H \H]))))

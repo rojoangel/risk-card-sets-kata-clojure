72e07161e1c29eb4e7a96f879cc9c92d02962098
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-01 22:00:59 +0100

    reorganize functions in collection ns

--- src/risk_cards/collection.clj
+++ src/risk_cards/collection.clj
@@ -1,6 +1,12 @@
 (ns risk-cards.collection
   (:require [risk-cards.set :as set]))
 
+(defn- some-joker? [collection]
+  (some set/joker? collection))
+
+(defn- only-two-symbols? [collection]
+  (= 2 (count (distinct collection))))
+
 (defmulti contains-valid-set? count)
 
 (defmethod contains-valid-set? 0 [_]
@@ -14,12 +20,6 @@
 
 (defmethod contains-valid-set? 3 [collection]
   (set/valid? collection))
-
-(defn- some-joker? [collection]
-  (some set/joker? collection))
-
-(defn- only-two-symbols? [collection]
-  (= 2 (count (distinct collection))))
 
 (defmethod contains-valid-set? 4 [collection]
   ((some-fn some-joker? (complement only-two-symbols?)) collection))

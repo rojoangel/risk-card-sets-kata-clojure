7216b88bf0edc00928f1f363c1d509b9de7aea7f
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-01 21:10:01 +0100

    Add more tests with 3 cards collections for completeness

--- test/risk_cards/collection_test.clj
+++ test/risk_cards/collection_test.clj
@@ -14,4 +14,8 @@
     (is (= false (collection/contains-valid-set? [\H \S]))))
   (testing "A collection of 3 cards should"
     (testing "contain a valid set if all cards are equal"
-      (is (= true (collection/contains-valid-set? [\H \H \H]))))))
+      (is (= true (collection/contains-valid-set? [\H \H \H]))))
+    (testing "contain a valid set if all cards are different"
+      (is (= true (collection/contains-valid-set? [\H \S \C]))))
+    (testing "not contain a valid set if not all cards equal or different"
+      (is (= false (collection/contains-valid-set? [\H \H \S]))))))
